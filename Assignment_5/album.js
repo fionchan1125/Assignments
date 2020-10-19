9437f6c161d96481e8316bd1735839c766660909
Fion Chan <70460596+fionchan1125@users.noreply.github.com>
2020-10-19 22:52:01 +0800

    05

---
+++ Assignment_5/album.js
@@ -0,0 +1,5 @@
+function table(){
+    var selecter = parent.document.getElementById("selecter");
+                                if (selecter.value=="0"){document.getElementById("iframe").src=src="https://airtable.com/embed/shrCt9d5ZxsEhSYf3?backgroundColor=yellow"};
+                                if (selecter.value=="1"){document.getElementById("iframe").src="https://airtable.com/embed/shrZZdM9qqERDwTTT?backgroundColor=yellow"};
+                                if (selecter.value=="2"){document.getElementById("iframe").src="https://airtable.com/embed/shr1Yh6IqmymELGoJ?backgroundColor=yellow"};}
