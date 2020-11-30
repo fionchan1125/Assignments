31767b400bb5434d0d2e5279afa71d53a9aa47ea
Fion Chan <70460596+fionchan1125@users.noreply.github.com>
2020-11-30 15:01:48 +0800

    assignment09

---
+++ Assignment_9/salons.js
@@ -0,0 +1,14 @@
+function table(){
+    var selecter = parent.document.getElementById("selecter");
+                                if (selecter.value=="0"){document.getElementById("iframe").src="https://airtable.com/embed/shrO7hdX5LHJRuQ8y?backgroundColor=gray&layout=card"};
+                                if (selecter.value=="1"){document.getElementById("iframe").src="https://airtable.com/embed/shrKZ3EpoTKRzmLWz?backgroundColor=gray"};
+                                if (selecter.value=="2"){document.getElementById("iframe").src="https://airtable.com/embed/shrBDGfIzkWwyeQ67?backgroundColor=gray"};
+                                if (selecter.value=="3"){document.getElementById("iframe").src="https://airtable.com/embed/shrlS7UslZuX5xbpM?backgroundColor=gray"};
+                                if (selecter.value=="4"){document.getElementById("iframe").src="https://airtable.com/embed/shrg2A7lNAbFYMP6n?backgroundColor=gray"};                                
+                                if (selecter.value=="5"){document.getElementById("iframe").src="https://airtable.com/embed/shrJaSBXPYb1JnX83?backgroundColor=gray"};
+                                if (selecter.value=="6"){document.getElementById("iframe").src="https://airtable.com/embed/shrfFmS4p9XHmvvme?backgroundColor=gray"};                                
+                                if (selecter.value=="7"){document.getElementById("iframe").src="https://airtable.com/embed/shrwzMHC5HeogFA3v?backgroundColor=gray"};
+                                if (selecter.value=="8"){document.getElementById("iframe").src="https://airtable.com/embed/shrER4uRUNSZHfFKp?backgroundColor=gray"}; 
+                                if (selecter.value=="9"){document.getElementById("iframe").src="https://airtable.com/embed/shrdGtySLrCMBeND4?backgroundColor=gray"};                               
+        
+                         }
