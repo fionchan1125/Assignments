fc26b966df5cd972fe13596a289ba34def10bbc3
Fion Chan <70460596+fionchan1125@users.noreply.github.com>
2020-12-08 11:05:47 +0800

    update

--- final/hairdye.js
+++ final/hairdye.js
@@ -2,4 +2,7 @@ function table(){
     var selecter = parent.document.getElementById("selecter");
                                 if (selecter.value=="0"){document.getElementById("iframe").src=src="https://airtable.com/embed/shrjfrTj7DcvrFEJP?backgroundColor=gray"};
                                 if (selecter.value=="1"){document.getElementById("iframe").src="https://airtable.com/embed/shrg1xrCIYXUl1hxW?backgroundColor=gray"};
-                                if (selecter.value=="2"){document.getElementById("iframe").src="https://airtable.com/embed/shrdjZCxGS76mppwy?backgroundColor=gray"};}
+                                if (selecter.value=="2"){document.getElementById("iframe").src="https://airtable.com/embed/shrdjZCxGS76mppwy?backgroundColor=gray"};
+                                if (selecter.value=="3"){document.getElementById("iframe").src="https://airtable.com/embed/shrajc8naBYZZ8P7L?backgroundColor=gray&viewControls=on"};
+                                
+                            }
